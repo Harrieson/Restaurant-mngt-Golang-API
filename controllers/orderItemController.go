f6b4d0d7989975fe01fd7821097bddf1bca95793
Harrieson <harriemburu@gmail.com>
2023-02-18 14:16:50 +0300

    Middlewares

---
+++ controllers/orderItemController.go
@@ -0,0 +1,31 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func GetOrderItems() gin.HandlerFunc{
+	return func(ctx *gin.Context) {}
+}
+
+
+func GetOrderItemsByOrder() gin.HandlerFunc{
+	return func(ctx *gin.Context) {}
+}
+
+func ItemsByOrder(id string)(orderItems []primitive.M, err error){
+
+}
+
+func GetOrderItem() gin.HandlerFunc{
+	return func(ctx *gin.Context) {}
+}
+
+func UpdateOrderItem() gin.HandlerFunc{
+	return func(ctx *gin.Context) {}
+}
+
+func CreateOrderItem() gin.HandlerFunc{
+	return func(ctx *gin.Context) {}
+}
---
+++ routes/menuRouter.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	controller "restaurant-manmnt/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func MenuRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/menus", controller.GetMenus())
+	incomingRoutes.GET("/menus/:menu_id", controller.GetMenu())
+	incomingRoutes.POST("/menus", controller.CreateMenu())
+	incomingRoutes.PATCH("/menu/:menu_id", controller.UpdateMenu())
+}
